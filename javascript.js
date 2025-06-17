966f4d503932902fc95bd164eb412a942c482294
aleynnanq <aleynnaquinones@gmail.com>
2025-06-17 09:50:02 -0400

    Create button input to change grid size

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+const button = document.querySelector("#sizeButton");
+
 function makeGrids(size) {
   const screen = document.querySelector(".sketch-screen");
   for (let i = 0; i < size; i++) {
@@ -13,12 +15,31 @@ function makeGrids(size) {
   }
 }
 
-makeGrids(16);
+function destroyGrids() {
+  const screen = document.querySelector(".sketch-screen");
+  screen.innerHTML = "";
+}
 
-const cells = document.querySelectorAll(".cell");
-
-cells.forEach((cell) => {
-  cell.addEventListener("mouseenter", () => {
-    cell.setAttribute("style", "background-color:black;");
+function attachEventListeners() {
+  const cells = document.querySelectorAll(".cell");
+  cells.forEach((cell) => {
+    cell.addEventListener("mouseenter", () => {
+      cell.setAttribute("style", "background-color:black;");
+    });
   });
+}
+
+button.addEventListener("click", () => {
+  const userAnswer = prompt("Type a number between 0 and 100", "16");
+  if (isNaN(userAnswer) || Number(userAnswer) < 0 || Number(userAnswer) > 100) {
+    alert("An error occurred! Please enter a number between 0 and 100");
+    return;
+  }
+
+  destroyGrids();
+  makeGrids(Number(userAnswer));
+  attachEventListeners();
 });
+
+makeGrids(16);
+attachEventListeners();
